e107bbe2f22e843060ea465de90000a102b3d500
raydrawc <raydraw@163.com>
2018-09-21 17:26:44 +0800

    多平台测试

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,28 +18,11 @@ get_env_os(){
 }
 
 PWD=`pwd`
+## install boost
 
+## install luajit
 git clone http://luajit.org/git/luajit-2.0.git luajit
 cd luajit
 git checkout v2.1
-make install
-
-BOOST_DL_PATH=
-BOOST=boost_1_68_0
-
-LUA_DL_PATH=
-LUA=
-
-
-# copy dep
-git clone https://github.com/raydrawc/lua_nif_dep.git deps
-cd deps && ./decode.sh
-
-# if you use luajit and you can use this
-cd lua/src/ && make CFLAGS+="-shared -fPIC"
-# if you want to use .a and use this
-rm -f libluajit.so
-
-# if you use lua.org and you can use this
-# get_env_os
-# cd lua/src/ %% make ${PLATFORM} -cxxflag="-shared -fPIC"
+sudo make install CFLAGS+="-shared -fPIC"
+sudo ldconfig
