18846f6a8a4bf6019940146c1293075c1a3cc77f
raydrawc <raydraw@163.com>
2018-09-29 11:55:14 +0800

    更新脚本

--- bootstrap.sh
+++ bootstrap.sh
@@ -34,8 +34,9 @@ cd ..
 wget http://luajit.org/download/LuaJIT-2.1.0-beta3.tar.gz
 tar -zxv -f LuaJIT-2.1.0-beta3.tar.gz
 mv LuaJIT-2.1.0-beta3 luajit
-cd luajit/src && make CXXFLAGS="-shared -fPIC"
-rm -f libluajit.so
+cd luajit/src
+make install CXXFLAGS="-shared -fPIC"
+echo /usr/local/lib > /etc/ld.so.conf.d/usrlocal.conf
 
 
 ## install luajit
