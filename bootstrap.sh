de1e1d723c4eeb57cad95d6dfd0c37260629bd63
raydrawc <raydraw@163.com>
2018-10-16 10:31:43 +0800

    fix address luajit

--- bootstrap.sh
+++ bootstrap.sh
@@ -31,7 +31,7 @@ cd boost && ./bootstrap.sh
 ./b2 stage --with-thread link=static threading=multi runtime-link=static cxxflags="-shared -fPIC"
 
 cd ..
-wget http://luajit.org/download/LuaJIT-2.1.0-beta3.tar.gz
+wget https://luajit.org/download/LuaJIT-2.1.0-beta3.tar.gz
 tar -zxv -f LuaJIT-2.1.0-beta3.tar.gz
 mv LuaJIT-2.1.0-beta3 luajit
 cd luajit/src
