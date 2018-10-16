6a3b148025bcd04f29fd0ef573724d1f43fcbcbe
raydrawc <raydraw@163.com>
2018-10-16 10:48:39 +0800

    fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -35,7 +35,7 @@ wget https://luajit.org/download/LuaJIT-2.1.0-beta3.tar.gz
 tar -zxv -f LuaJIT-2.1.0-beta3.tar.gz
 mv LuaJIT-2.1.0-beta3 luajit
 cd luajit/src
-make install CXXFLAGS="-shared -fPIC"
+make install CFLAGS+="-shared -fPIC"
 echo /usr/local/lib > /etc/ld.so.conf.d/usrlocal.conf
 
 
