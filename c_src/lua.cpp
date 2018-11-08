0d1ce5fb58c44b8c3076488f68a81aadca922974
raydrawc <raydraw@163.com>
2018-11-08 11:11:10 +0800

    fix bug

--- c_src/lua.cpp
+++ c_src/lua.cpp
@@ -4,22 +4,22 @@
 #include "lua_utils.hpp"
 
 static int traceback (lua_State *L) {
-  if (!lua_isstring(L, 1))  /* 'message' not a string? */
-    return 1;  /* keep it intact */
-  lua_getglocal(L, "debug");
-  if (!lua_istable(L, -1)) {
-    lua_pop(L, 1);
+     if (!lua_isstring(L, 1))  /* 'message' not a string? */
+         return 1;  /* keep it intact */
+    lua_getglobal(L, "debug");
+    if (!lua_istable(L, -1)) {
+        lua_pop(L, 1);
+        return 1;
+    }
+    lua_getfield(L, -1, "traceback");
+    if (!lua_isfunction(L, -1)) {
+        lua_pop(L, 2);
+        return 1;
+    }
+    lua_pushvalue(L, 1);  /* pass error message */
+    lua_pushinteger(L, 2);  /* skip this function and traceback */
+    lua_call(L, 2, 1);  /* call debug.traceback */
     return 1;
-  }
-  lua_getfield(L, -1, "traceback");
-  if (!lua_isfunction(L, -1)) {
-    lua_pop(L, 2);
-    return 1;
-  }
-  lua_pushvalue(L, 1);  /* pass error message */
-  lua_pushinteger(L, 2);  /* skip this function and traceback */
-  lua_call(L, 2, 1);  /* call debug.traceback */
-  return 1;
 }
 
 namespace lua {
